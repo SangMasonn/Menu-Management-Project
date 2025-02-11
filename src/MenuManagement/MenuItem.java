2487db6784e91750b81ad4b5ccbb21dc115cbae2
SangMasonn <sanghuu0201@gmail.com>
2025-02-11 07:42:17 +0700

    Update MenuItem.java

--- src/MenuManagement/MenuItem.java
+++ src/MenuManagement/MenuItem.java
@@ -1,4 +1,8 @@
-package MenuManagement;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication35;
 
 import java.io.*;
 import java.util.*;
